084354f85b7a837de3a7ce0f5a1deb358b619018
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2022-09-29 19:55:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# More Bukkit 
+
+![Logo](/images/logo.png)
+
+## Index
+
+- [Index](#index);
+- [About](#about);
+- [Installation](#installation);
+- [Building](#building);
+- [Documentation](#documentation).
+
+## About
+
+This is a small java libarary providing additions to Bukkit API.
+
+## Installation
+
+First, add MoonTalk repository to your pom.xml:
+
+```xml
+<repository>
+    <id>moontalk</id>
+    <url>httsp://repo.moontalk.space/repository/maven-releases/</url>
+</repository>
+```
+
+Second, add library as dependency:
+
+```xml
+<dependency>
+    <groupId>space.moontalk.mc.more</groupId>
+    <artifactId>bukkit</artifactId>
+    <version>1.0.0</version>
+</dependency>
+```
+
+Done.
+
+## Building
+
+Just execute the following Maven command in your terminal:
+
+```bash
+mvn install
+```
+
+## Documentation
+
+Will be added soon...
---
+++ src/main/java/space/moontalk/mc/more/bukkit/MoreBukkit.java
@@ -0,0 +1,111 @@
+package space.moontalk.mc.more.bukkit;
+
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import org.jetbrains.annotations.NotNull;
+
+import lombok.val;
+
+public interface MoreBukkit {
+    static final int SECOND_TICKS = 20;
+    static final int MINUTE_TICKS = 60 * SECOND_TICKS;
+    static final int HOUR_TICKS   = 60 * MINUTE_TICKS;
+
+    static int getCurrentSecond() {
+        return Bukkit.getCurrentTick() / SECOND_TICKS;
+    }
+
+    static int getCurrentMinute() {
+        return Bukkit.getCurrentTick() / MINUTE_TICKS;
+    }
+
+    static int getCurrentHour() {
+        return Bukkit.getCurrentTick() / HOUR_TICKS;
+    }
+
+    static @NotNull World getWorld(@NotNull UUID worldUniqueId) {
+        val world = Bukkit.getWorld(worldUniqueId);
+
+        if (world == null) {
+            val message = String.format("world with uuid %s not found", worldUniqueId.toString());
+            throw new IllegalArgumentException(message);
+        }
+
+        return world;
+    }
+
+    static @NotNull World getWorld(@NotNull String worldName) {
+        val world = Bukkit.getWorld(worldName);
+
+        if (world == null) {
+            val message = String.format("world with name %s not found");
+            throw new IllegalArgumentException(message);
+        }
+
+        return world;
+    }
+
+    static @NotNull UUID getPlayerUniqueId(@NotNull String playerName) {
+        val player = Bukkit.getPlayer(playerName);
+
+        if (player != null) 
+            return player.getUniqueId();
+
+        val offlinePlayer = Bukkit.getOfflinePlayer(playerName);
+
+        if (offlinePlayer != null) 
+            return offlinePlayer.getUniqueId();
+
+        val message = String.format("not online neither offline player with name %s not found", playerName);
+        throw new IllegalArgumentException(message);
+    }
+
+    static @NotNull OfflinePlayer getOfflinePlayer(@NotNull UUID playerUniqueId) {
+        val player = Bukkit.getOfflinePlayer(playerUniqueId);
+
+        if (player == null) {
+            val message = String.format("offline player with uuid %s not found", playerUniqueId.toString());
+            throw new IllegalArgumentException(message);
+        }
+
+        return player;
+    }
+
+    static @NotNull OfflinePlayer getOfflinePlayer(@NotNull String playerName) {
+        val player = Bukkit.getOfflinePlayer(playerName);
+
+        if (player == null) {
+            val message = String.format("offline player with name %s not found", playerName);
+            throw new IllegalArgumentException(message);
+        }
+
+        return player;
+    }
+
+    static @NotNull Player getPlayer(@NotNull UUID playerUniqueId) {
+        val player = Bukkit.getPlayer(playerUniqueId);
+
+        if (player == null) {
+            val message = String.format("player with uuid %s not found", playerUniqueId.toString());
+            throw new IllegalArgumentException(message);
+        }
+
+        return player;
+    }
+
+    static @NotNull Player getPlayer(@NotNull String playerName) {
+        val player = Bukkit.getPlayer(playerName);
+
+        if (player == null) {
+            val message = String.format("player with name %s not found", playerName);
+            throw new IllegalArgumentException(message);
+        }
+
+        return player;
+    }
+}
