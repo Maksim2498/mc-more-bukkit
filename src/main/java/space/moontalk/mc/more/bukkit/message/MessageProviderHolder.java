17b8af46903e025b8f15427c173bb962f5d939d0
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2022-10-04 20:32:35 +0300

    Added MessageProviderHolder interface

---
+++ src/main/java/space/moontalk/mc/more/bukkit/message/MessageProviderHolder.java
@@ -0,0 +1,7 @@
+package space.moontalk.mc.more.bukkit.message;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface MessageProviderHolder {
+    @NotNull MessageProvider getMessageProvider();
+}
